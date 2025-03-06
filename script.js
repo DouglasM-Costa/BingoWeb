1684b342527f9ed3f322afaa138255059a23adff
Douglas <douglasmcosta1@gmail.com>
2025-03-06 15:50:57 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var option = document.querySelector("div#option")
+
+function start(){ // function for starting game
+    let play = confirm('Deseja começar uma nova rodada?')
+    if (play == true){
+        let list = []
+    }else{alert ('fica para uma próxima')}
+}
